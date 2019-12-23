b531dc8ef9836df2ba05d74b1820993402b2d136
liljenfeldt <henrik@varia.nu>
2019-12-23 16:05:17 +0100

    bug fix assembly type

--- create_database.sql
+++ create_database.sql
@@ -1,16 +1,15 @@
 --Script to generate the decay heat database
-drop database if exists decay_heat_verification
-create database if exists decay_heat_verification
-use decay_heat_verification
-create table if not exists assembly_type(
+create database if not exists decay_heat_verification;
+use decay_heat_verification;
+create table if not exists assembly_type (
     id int auto_increment not null primary key,
     assembly_type_name varchar(55)
-)
-create table if not exists assembly(
+);
+create table if not exists assembly (
     id int auto_increment not null primary key,
     assembly_name varchar(55),
     initial_enrichment float,
     burnup int,
-    assembly_type_id int not null
+    assembly_type_id int not null,
     foreign key (assembly_type_id) references assembly_type(id)
 );
