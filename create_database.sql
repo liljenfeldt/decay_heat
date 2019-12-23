b212d9cc5da313ab3a6099101f623f880545cad4
liljenfeldt <henrik@varia.nu>
2019-12-23 15:47:08 +0100

    Added auto increment to id in assembly

--- create_database.sql
+++ create_database.sql
@@ -2,7 +2,7 @@
 drop database if exists decay_heat_verification
 create database decay_heat_verification
 create table if not exists assembly(
-    id int,
+    id int auto_increment not null primary key,
     assembly_name varchar(55),
     initial_enrichment float,
     burnup int
