59a57473a8e18f0e62dd38d073a850d3bfef6996
liljenfeldt <henrik@varia.nu>
2019-12-24 12:17:12 +0100

    Added data import from file

--- create_database.sql
+++ create_database.sql
@@ -1,3 +1,4 @@
+drop database if exists decay_heat_verification;
 create database if not exists decay_heat_verification;
 use decay_heat_verification;
 create table if not exists assembly_type (
@@ -9,6 +10,15 @@ create table if not exists assembly (
     assembly_name varchar(55),
     initial_enrichment float,
     burnup int,
-    assembly_type_id int not null,
+    assembly_type_id int,
     foreign key (assembly_type_id) references assembly_type(id)
 );
+LOAD DATA LOCAL INFILE '/home/noemi/python/decay_heat/decay_heat/epri_measure.csv'
+    INTO TABLE assembly
+    COLUMNS TERMINATED BY ';'
+    ENCLOSED BY '"' 
+    LINES TERMINATED BY '\n' 
+    IGNORE 1 LINES
+    (@dummy,@dummy,@dummy,assembly_name,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy) 
+    ;
+SELECT * FROM assembly;
